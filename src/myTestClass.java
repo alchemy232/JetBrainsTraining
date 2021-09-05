934cc704847aebac1ac74429bd845db9a9cba039
alchemy232 <mr.alchemy@gmail.com>
2021-09-05 19:11:23 +0300

    1st try

--- src/myTestClass.java
+++ src/myTestClass.java
@@ -1,22 +1,5 @@
 public class myTestClass {
 
 
-    public static void main(String[] args) {
-        StringBuilder sb1 = new StringBuilder();
-        sb1 = sb1.append("Behold the power of StringBuilder");
-
-        StringBuilder sb2 = new StringBuilder();
-        sb2.append("Behold the power of StringBuilder");
-
-        StringBuilder sb3 = new StringBuilder()
-                .append("Behold")
-                .append("the power of")
-                .append("StringBuilder");
-
-        StringBuilder sb4 = new StringBuilder("Behold the power of StringBuilder");
-        System.out.println(sb1);
-        System.out.println(sb2);
-        System.out.println(sb3);
-        System.out.println(sb4);
-    }
 }
+
